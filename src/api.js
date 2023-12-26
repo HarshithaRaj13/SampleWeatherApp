85683941bd045c1b54e83c0e82a94481b342b637
HarshithaRaj13 <harshitharajshekar13@gmail.com>
2023-12-26 20:06:20 +0530

    commit

---
+++ src/api.js
@@ -0,0 +1,8 @@
+export const WeatherApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-key": "d5e6017b874a1d8c605482310ca9b12d",
+    "X-RapidAPI-Host": "wft-geo-dblClick.p.rapidapi.com",
+  },
+};
+export const Openurl = "https://openweathermap.org/api";
---
+++ src/components/search/search.js
@@ -0,0 +1,49 @@
+// Weather.js
+import React, { useState } from "react";
+import axios from "axios";
+
+const Weather = () => {
+  const [city, setCity] = useState("");
+  const [weatherData, setWeatherData] = useState(null);
+  const [error, setError] = useState(null);
+
+  const API_KEY = "90446f7b6558d963594769ea518d6a09"; // Replace with your OpenWeatherMap API key
+
+  const getWeatherData = async () => {
+    try {
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
+      );
+      setWeatherData(response.data);
+      setError(null);
+    } catch (err) {
+      setError("City not found. Please try again.");
+      setWeatherData(null);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Weather App</h1>
+      <input
+        type="text"
+        placeholder="Enter city"
+        value={city}
+        onChange={(e) => setCity(e.target.value)}
+      />
+      <button onClick={getWeatherData}>Get Weather</button>
+
+      {error && <p>{error}</p>}
+
+      {weatherData && (
+        <div>
+          <h2>{weatherData.name}</h2>
+          <p>Temperature: {weatherData.main.temp} K</p>
+          <p>Weather: {weatherData.weather[0].description}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Weather;
